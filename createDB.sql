0f147d827f5b8d0d55ced322ea088f6c2f4c2f3b
Alexei Kurbatov <vinsler@yandex.ru>
2019-01-14 23:28:45 +0300

    add tables in db

---
+++ createDB.sql
@@ -0,0 +1,19 @@
+create database if not exists recordtask default character set utf8 collate utf8_general_ci;
+
+use recordtask;
+
+drop table if exists record;
+create table if not exists record (
+
+  id              int(11) auto_increment, primary key(id),
+  `date`          date not null,
+  task            int (12) not null unique,
+  client          varchar (40) not null,
+  address         varchar (80) not null,
+  
+  utp             int (4),
+  category        int (2),
+  sum             int (7),
+  description     varchar (160)
+
+) default character set utf8;
